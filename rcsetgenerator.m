cb2392923148099e7b04831c1a433710995799e5
dell <yc992147156@me.com>
2020-08-03 11:07:44 +0800

    Update

--- rcsetgenerator.m
+++ rcsetgenerator.m
@@ -5,7 +5,7 @@ CFStringRef bundleID = CFSTR("com.michael.generator");
 int main() {
     int ret = 0;
     CFArrayRef keyList = CFPreferencesCopyKeyList(bundleID, CFSTR("mobile"), kCFPreferencesAnyHost);
-    if (keyList == nil || !CFArrayContainsValue(keyList, CFRangeMake(0, CFArrayGetCount(keyList)), CFSTR("enabled")) || CFBooleanGetValue(CFPreferencesCopyValue(CFSTR("enabled"), bundleID, CFSTR("mobile"), kCFPreferencesAnyHost))) {
+    if (keyList == NULL || !CFArrayContainsValue(keyList, CFRangeMake(0, CFArrayGetCount(keyList)), CFSTR("enabled")) || CFBooleanGetValue(CFPreferencesCopyValue(CFSTR("enabled"), bundleID, CFSTR("mobile"), kCFPreferencesAnyHost))) {
         int status = system("setgenerator");
         ret = WEXITSTATUS(status);
     }

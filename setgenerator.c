994aec3375bfee31713bd3c004caaf74b75de113
macOS <macos@iMac.local>
2021-03-05 22:54:33 +0800

    Update

--- setgenerator.c
+++ setgenerator.c
@@ -78,8 +78,20 @@ int main(int argc, char **argv) {
                 }
                 dimentio_term();
             }
+            CFArrayRef keyList = CFPreferencesCopyKeyList(bundleID, CFSTR("mobile"), kCFPreferencesAnyHost);
+            if (keyList != NULL) {
+                if (CFArrayContainsValue(keyList, CFRangeMake(0, CFArrayGetCount(keyList)), CFSTR("enabled")) && !CFBooleanGetValue(CFPreferencesCopyValue(CFSTR("enabled"), bundleID, CFSTR("mobile"), kCFPreferencesAnyHost))) {
+                    CFRelease(keyList);
+                    printf("The program will NOT run automatically during the next jailbreak.\n");
+                } else {
+                    CFRelease(keyList);
+                    printf("The program will run automatically during the next jailbreak.\n");
+                }
+            } else {
+                printf("The program will run automatically during the next jailbreak.\n");
+            }
             char *generator = getGenerator();
-            printf("The currently set generator is %s.\n", generator);
+            printf("When next time the program is running, the generator will be set to %s.\n", generator);
             free(generator);
             return 0;
         } else if (!vaildGenerator(argv[1])) {
